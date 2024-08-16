d78ad7d168e4002efb03c91da9f9a79dcb56a0dc
Abhishek Mewada <abhishek.engineer3@gmail.com>
2024-08-16 13:28:45 +0530

    center the box

--- Snake.js
+++ Snake.js
@@ -11,8 +11,8 @@ let foodCell = generateRandomCell();
 // Function to generate a random position for the food
 function generateRandomCell() {
     return [
-        Math.round((Math.random()*(1450))/cell)*cell,
-        Math.round((Math.random()*(750))/cell)*cell
+        Math.round((Math.random()*(450))/cell)*cell,
+        Math.round((Math.random()*(950))/cell)*cell
       ]
 }
 
@@ -42,7 +42,7 @@ function draw() {
         ctx.fillText('Game Over', 100, 100);
         return;
     }
-    ctx.clearRect(0,0,1500,800);
+    ctx.clearRect(0,0,1000,500);
 
     // Draw the snake
     for(let i of snakeCells) {
@@ -75,13 +75,13 @@ function update() {
     }
     else if(direction==='down') {
         headY = headY+cell;
-        if(headY==800) {
+        if(headY==500) {
             gameOver=true;
         }
     }
     else if(direction==='right') {
         headX = headX+cell;
-        if(headX==1500) {
+        if(headX==1000) {
             gameOver=true;
         }
     }
